7a3961956f48681197522663b65e8534039ab1b1
Neoasp <jeremydelsol@gmail.com>
2019-12-09 15:49:59 +0100

    lol

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/ClientService.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/ClientService.java
@@ -3,12 +3,12 @@ package com.formation.LeonNettoyage.services.impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formation.LeonNettoyage.persistence.entities.Client;
 import com.formation.LeonNettoyage.services.IClientService;
-
-
+import com.formation.LeonNettoyage.services.common.AbstractService;
 
 @Service
 @Transactional
-public class ClientService implements IClientService{
+public class ClientService extends AbstractService<Client> implements IClientService{
 
 }
