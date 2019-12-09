d2d76101c8c5592965351dea57ef02560b0f2fda
brichier <benjaminrichier@yahoo.fr>
2019-12-09 15:36:01 +0100

    ben 1° commit

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/ClientService.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/ClientService.java
@@ -3,12 +3,12 @@ package com.formation.LeonNettoyage.services.impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.formation.persistence.entities.Client;
-import com.formation.services.IClientService;
-import com.formation.services.common.AbstractService;
+import com.formation.LeonNettoyage.services.IClientService;
+
+
 
 @Service
 @Transactional
-public class ClientService extends AbstractService<Client> implements IClientService{
+public class ClientService implements IClientService{
 
 }
