20636f0b36036fcb2e13f782a8ef48f410a9a8b2
brichier <benjaminrichier@yahoo.fr>
2019-12-10 13:07:21 +0100

    Attempt et status service

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/AttemptService.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/AttemptService.java
@@ -1,10 +1,26 @@
 package com.formation.LeonNettoyage.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formation.LeonNettoyage.persistence.entities.Attempt;
+import com.formation.LeonNettoyage.persistence.repositories.IAttemptRepository;
+import com.formation.LeonNettoyage.services.IAttemptService;
+import com.formation.LeonNettoyage.services.common.AbstractService;
+
 @Service
 @Transactional
-public class AttemptService {
+public class AttemptService extends AbstractService<Attempt> implements IAttemptService{
+
+	@Autowired
+	IAttemptRepository repo;
+	
+	
+	@Override
+	public JpaRepository<Attempt, Long> getRepo() {
+		return repo;
+	}
 
 }
