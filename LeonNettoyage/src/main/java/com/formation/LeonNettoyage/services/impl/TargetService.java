82bf0f92eb69b73d692e03c4d8c15261d9a5e7c8
nathan <nathan_dn@hotmail.com>
2019-12-10 12:52:17 +0100

    targetservice

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/TargetService.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/TargetService.java
@@ -1,10 +1,21 @@
 package com.formation.LeonNettoyage.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formation.LeonNettoyage.persistence.entities.Target;
+import com.formation.LeonNettoyage.persistence.repositories.ITargetRepository;
+import com.formation.LeonNettoyage.services.common.AbstractService;
+
 @Service
 @Transactional
-public class TargetService {
-
+public class TargetService extends AbstractService<Target> {
+	@Autowired
+	private ITargetRepository repo;
+	
+	public JpaRepository<Target, Long> getRepo() {
+		return repo;
+	}
 }
