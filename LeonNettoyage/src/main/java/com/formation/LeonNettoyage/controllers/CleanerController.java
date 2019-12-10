768da903e563715bcc3146c9e0a0a671faf6ce44
Lucas <lucasmarin115@gmail.com>
2019-12-10 12:43:11 +0100

    gg

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/controllers/CleanerController.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/controllers/CleanerController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.formation.LeonNettoyage.dto.ClientLight;
+import com.formation.LeonNettoyage.dto.CleanerFull;
+import com.formation.LeonNettoyage.dto.CleanerLight;
 import com.formation.LeonNettoyage.services.ICleanerService;
 
 @RestController
@@ -26,3 +27,22 @@ public class CleanerController {
 		mapper = new ModelMapper();
 	}
 	
+	@RequestMapping(path = "/listLight", method = RequestMethod.GET) 
+	public List<CleanerLight> findAllLight() {
+		
+		return service.findAll()
+				.stream()
+				.map(c -> mapper.map(c, CleanerLight.class))
+				.collect(Collectors.toList());
+	}
+	
+	@RequestMapping(path = "/listFull", method = RequestMethod.GET) 
+	public List<CleanerFull> findAllFull() {
+		
+		return service.findAll()
+				.stream()
+				.map(c -> mapper.map(c, CleanerFull.class))
+				.collect(Collectors.toList());
+}
+	
+}
