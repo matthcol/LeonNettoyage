a02c8846b195b1b722f699ef5e37ecd996b9e93f
nathan <nathan_dn@hotmail.com>
2019-12-11 12:06:34 +0100

    getCurrentCleaner etc...

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/IAuthChecker.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/IAuthChecker.java
@@ -1,9 +1,10 @@
 package com.formation.LeonNettoyage.services;
 
+import com.formation.LeonNettoyage.persistence.entities.Cleaner;
 import com.formation.LeonNettoyage.persistence.entities.Client;
 
 public interface IAuthChecker {
 
 	Client getCurrentUser();
-
+	Cleaner getCurrentCleaner();
 }
--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/AuthChecker.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/services/impl/AuthChecker.java
@@ -6,8 +6,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import com.formation.LeonNettoyage.exception.NotAuthorizedException;
+import com.formation.LeonNettoyage.persistence.entities.Cleaner;
 import com.formation.LeonNettoyage.persistence.entities.Client;
 import com.formation.LeonNettoyage.services.IAuthChecker;
+import com.formation.LeonNettoyage.services.ICleanerService;
 import com.formation.LeonNettoyage.services.IClientService;
 
 
@@ -18,12 +20,26 @@ public class AuthChecker implements IAuthChecker {
 	@Autowired
 	private IClientService userService;
 	
+	@Autowired
+	private ICleanerService userCleanerService;
+	
 	@Override
 	public Client getCurrentUser() {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		if (principal instanceof UserDetails) {
 			UserDetails ud = (UserDetails)principal;
 			return userService.findByMail(ud.getUsername());
+		} else {
+			throw new NotAuthorizedException("NO CURRENT USER");
+		}
+	}
+	
+	@Override
+	public Cleaner getCurrentCleaner() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+			UserDetails ud = (UserDetails)principal;
+			return userCleanerService.findByName(ud.getUsername());
 		} else {
 			throw new NotAuthorizedException("NO CURRENT USER");
 		}
