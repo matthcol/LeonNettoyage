a56fa2c121eb51327fb8635d229d26d422aeb5fd
Neoasp <jeremydelsol@gmail.com>
2019-12-11 12:50:29 +0100

    fgdfd

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/persistence/entities/SkillOfCleaner.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/persistence/entities/SkillOfCleaner.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-@Table(name="serviceOfCleaner")
+@Table(name="skillOfCleaner")
 @Entity
 public class SkillOfCleaner {
 	
