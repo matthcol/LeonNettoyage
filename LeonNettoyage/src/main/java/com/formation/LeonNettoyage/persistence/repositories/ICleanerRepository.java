42148213ce3ace95eeeb913e5ba72d091c56233d
nathan <nathan_dn@hotmail.com>
2019-12-11 11:46:13 +0100

    some changes

--- LeonNettoyage/src/main/java/com/formation/LeonNettoyage/persistence/repositories/ICleanerRepository.java
+++ LeonNettoyage/src/main/java/com/formation/LeonNettoyage/persistence/repositories/ICleanerRepository.java
@@ -7,5 +7,5 @@ import com.formation.LeonNettoyage.persistence.entities.Cleaner;
 
 public interface ICleanerRepository extends JpaRepository<Cleaner,Long>  {
 	@Query(nativeQuery = true, value = "SELECT * FROM cleaner WHERE pseudo = ?1 LIMIT 0,1")
-	Cleaner findByMail(String username);
+	Cleaner findByName(String username);
 }
